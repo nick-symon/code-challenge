77df8e9f6655fe2b206f6f82c7db48b9a44121fc
nick-symon <symon.nick@gmail.com>
2020-10-12 18:09:33 -0500

    updates based on code review

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,17 +6,16 @@ import usaddress
 def test_api_parse_succeds(client):
     address_string = '123 main st chicago il'
     test_address_components, test_address_type = usaddress.tag(address_string)
-    get_param = urlencode({'address': address_string}) 
 
-    response = client.get(f'/api/parse/?{get_param}')
+    response = client.get('/api/parse/', {'address': address_string} )
     assert response.status_code == 200
-    assert response.data['input_string'] == '123 main st chicago il' 
+    assert response.data['input_string'] == address_string 
     assert response.data['address_components'] == test_address_components
     assert response.data['address_type'] == test_address_type
 
 def test_api_parse_raises_error(client):
     address_string = '123 main st chicago il 123 main st'
-    get_param = urlencode({'address': address_string}) 
-    response = client.get(f'/api/parse/?{get_param}')
+    response = client.get('/api/parse/', {'address': address_string})
+
     assert response.status_code == 400
     assert response.data['detail'] == "Unable to parse this value due to repeated labels. Please correct and resubmit"
